485e1f30ddf6dc836acd9c230636c9bc6546b1fd
Stephane Nicoll <snicoll@vmware.com>
2022-03-07 14:50:51 +0100

    Switch to Spring Boot 3
    This commit switches the build to Spring Boot 3.0.0-SNAPSHOT and Spring
    Framework 6.0.0-SNAPSHOT. Such an upgrade requires to bump the build to
    Java 17 and adapt to Jakarta EE9.

--- spring-aot/src/test/java/org/springframework/context/annotation/BuildTimeBeanDefinitionsRegistrarTests.java
+++ spring-aot/src/test/java/org/springframework/context/annotation/BuildTimeBeanDefinitionsRegistrarTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019-2021 the original author or authors.
+ * Copyright 2019-2022 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -62,6 +62,7 @@ class BuildTimeBeanDefinitionsRegistrarTests {
 	private static final String[] ANNOTATION_INFRASTRUCTURE_BEAN_NAMES = new String[] {
 			"org.springframework.context.annotation.internalAutowiredAnnotationProcessor",
 			"org.springframework.context.annotation.internalCommonAnnotationProcessor",
+			"org.springframework.context.annotation.internalJsr250AnnotationProcessor",
 			"org.springframework.context.annotation.internalPersistenceAnnotationProcessor",
 			"org.springframework.context.event.internalEventListenerProcessor",
 			"org.springframework.context.event.internalEventListenerFactory" };
